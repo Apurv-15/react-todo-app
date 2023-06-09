02fc83ff163c799e484b8bd00b0abf0eb7dc54bf
Apurv-15 <95355502+Apurv-15@users.noreply.github.com>
2023-06-09 15:43:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const sum = 1 + 1;
+  const [todos, setData] = useState(['Heelo', 'bye']);
+  const [input, setInput] = useState('');
+
   return (
-    <div>
-      <h1>Hello StackBlitz! ok</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <h1>hello apurv</h1>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        {/* <button onClick={()=>setData(data+1)}>update data</button> */}
+        <button>Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
